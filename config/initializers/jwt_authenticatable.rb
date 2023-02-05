817db92788678ac9025b45c1b75c72cc14e41649
Leon Siqueira <leon.siqueir4@gmail.com>
2023-02-05 15:39:00 -0300

    feat: custom errors

--- config/initializers/jwt_authenticatable.rb
+++ config/initializers/jwt_authenticatable.rb
@@ -3,12 +3,12 @@ module Devise
     class JWTAuthenticatable < Base
       def authenticate!
         payload = JwtHeaderReaderHelper.decoded_token(request)
-        return fail(:invalid) unless payload.present?
+        return fail!(:timeout) if payload == :expired
 
-        return fail(:invalid) if payload == :expired
+        return fail!(:invalid) unless payload.present?
 
         resource = mapping.to.find(payload['user_id'])
-        return fail(:not_found_in_database) unless resource
+        return fail!(:not_found_in_database) unless resource
 
         success!(resource)
       end
