98a73637013b69a9cb283feea9d4b4ebb26c8584
Leon Siqueira <leon.siqueir4@gmail.com>
2023-03-02 23:05:16 -0300

    chore: model annotation and seed

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: bids
+#
+#  id         :bigint           not null, primary key
+#  auction_id :bigint           not null
+#  user_id    :bigint           not null
+#  value      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Bid < ApplicationRecord
   belongs_to :auction
   belongs_to :user
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'jon.doe@email.com', password: 'pass1234', balance: 100_000)
