9f70ef1558b5946a1344a3331611a6148b4ff492
Leon Siqueira <leon.siqueir4@gmail.com>
2023-03-13 17:33:19 -0300

    refactor: nil-safe method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def current_user
-    @current_user ||= User.where(id: JwtHeaderReaderHelper.decoded_token(request)['user_id']).first
+    @current_user ||= User.where(id: JwtHeaderReaderHelper.decoded_token(request)&.dig('user_id')).first
   end
 end
