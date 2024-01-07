198c70f0e04ea794d1fdd111ad1b46b6394981e9
Leon Siqueira <leon.siqueir4@gmail.com>
2024-01-06 21:44:36 -0300

    feat: bid custom validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -25,6 +25,11 @@ class Transaction < ApplicationRecord
   validate :allowed_receivers_per_kind
   validate :allowed_givers_per_kind
 
+  validate :auction_in_progress, if: -> { kind == 'bid' }
+  validate :minimal_bid_reached, if: -> { kind == 'bid' }
+  validate :winning_bid_covered, if: -> { kind == 'bid' }
+  validate :enough_balance, if: -> { giver_type == 'User' }
+
   enum :kind, KINDS
 
   def allowed_receivers_per_kind
@@ -51,5 +56,30 @@ class Transaction < ApplicationRecord
             end
 
     errors.add(:giver_type, 'is not suitable for this kind of transaction') unless valid
+  end
+
+  def auction_in_progress
+    return unless Time.zone.now > receiver.end_date
+
+    errors.add(:auction, "is already over. You can't place bids in it anymore")
+  end
+
+  def minimal_bid_reached
+    return unless receiver.bids.empty? && (amount < receiver.minimal_bid)
+
+    errors.add(:amount, "is less than the minimal bid setted for this auction: #{receiver.minimal_bid}")
+  end
+
+  def winning_bid_covered
+    return unless receiver.bids.any? && (receiver < receiver.winning_bid.value + 10)
+
+    errors.add(:amount,
+               "needs to be greater than #{receiver.winning_bid.value + 10} to cover the currently winning bid")
+  end
+
+  def enough_balance
+    return unless giver.balance <= amount
+
+    errors.add(:amount, 'is greater than your current balance')
   end
 end
