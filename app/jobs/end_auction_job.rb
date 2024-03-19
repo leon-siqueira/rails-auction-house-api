402401fea0c9d92bab4795a6b6eabd25a0982413
Leon Siqueira <leon.siqueir4@gmail.com>
2024-03-18 23:37:34 -0300

    feat: polymorphic transaction on auction ending

--- app/jobs/end_auction_job.rb
+++ app/jobs/end_auction_job.rb
@@ -3,12 +3,10 @@ class EndAuctionJob < ApplicationJob
 
   def perform(auction)
     auction.update(status: :finished)
-    auction_owner ||= auction.user
-    winner_bid ||= auction.winning_bid
     return unless auction.bids.any?
 
-    auction.art.update(owner: winner_bid.user)
-    Transaction.create(kind: :auction_income, giver: auction, value: winner_bid.value, receiver: auction_owner)
-    auction_owner.update_balance
+    winner_bid ||= auction.winning_bid
+    auction.art.update(owner: winner_bid.giver)
+    Transaction.create(kind: :auction_income, giver: auction, amount: winner_bid.amount, receiver: auction.user)
   end
 end
