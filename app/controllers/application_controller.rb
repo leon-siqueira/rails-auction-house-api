4a62d7ed95f82edede9e244a07134f218bcb2370
Leon Siqueira <leon.siqueir4@gmail.com>
2024-03-23 18:08:30 -0300

    feat: setup for standard response on pundit error fix: undefined method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::API
   include Pundit::Authorization
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def current_user
-    @current_user ||= User.where(id: JwtHeaderReaderHelper.decoded_token(request)&.dig('user_id')).first
+    User.where(id: JwtHeaderReaderHelper.decoded_token(request).try(:dig, 'user_id')).first
   end
 
+  def user_not_authorized
+    render json: { error: 'You are not authorized to perform this action' }, status: :forbidden
+  end
 end
