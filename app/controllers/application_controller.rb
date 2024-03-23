645c740c31136e5b3071065e24d76664c80febb8
Leon Siqueira <leon.siqueir4@gmail.com>
2024-03-23 17:21:31 -0300

    chore: pundit:install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,8 @@
 class ApplicationController < ActionController::API
+  include Pundit::Authorization
+
   def current_user
     @current_user ||= User.where(id: JwtHeaderReaderHelper.decoded_token(request)&.dig('user_id')).first
   end
 
-  # TODO: change this by a proper authorization method like CanCanCan
-  def authorize_current_user(authorized_user)
-    if authorized_user == current_user
-      yield
-    elsif authorized_user != current_user || authorized_user.nil?
-      render json: { success: false, message: 'Unauthorized' }, status: :unauthorized
-    end
-  end
 end
