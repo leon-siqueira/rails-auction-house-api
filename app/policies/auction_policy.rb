9bcfafa4a9e0b12841c29eb982ccfa18dbe3da12
Leon Siqueira <leon.siqueir4@gmail.com>
2024-04-07 14:31:11 -0300

    fix: undefined method `art' for Auction:Class issue

--- app/policies/auction_policy.rb
+++ app/policies/auction_policy.rb
@@ -4,7 +4,7 @@ class AuctionPolicy
   def initialize(user, auction)
     @user = user
     @auction = auction
-    @art = auction.art
+    @art = auction.art if auction.instance_of?(Auction)
   end
 
   def index?
@@ -16,7 +16,7 @@ class AuctionPolicy
   end
 
   def create?
-    @user.is?(:admin) || @user.is?(:auctioneer)
+    @user.is?(:admin) || @user.is?(:auctioneer) && @user == @art.owner
   end
 
   def destroy?
