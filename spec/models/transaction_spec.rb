8b3bce679540ae7a7096ae8804021eaf97a75697
Leon Siqueira <leon.siqueir4@gmail.com>
2024-04-09 14:41:55 -0300

    test: bid specs moved to transaction

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -50,8 +50,65 @@ RSpec.describe Transaction, type: :model do
 
     it 'does not create and returns errors' do
       expect(subject).not_to be_valid
-      expect { subject.save }.not_to change { Transaction.all.count }
+      expect { subject.save }.not_to(change { Transaction.all.count })
       expect(subject.errors.messages).to eq(receiver_type: ['is not suitable for this kind of transaction'], giver_type: ['is not suitable for this kind of transaction'])
+    end
+  end
+
+  describe 'Bid transaction validations' do
+    subject { described_class.new(receiver: auction, giver: user, amount:, kind: 'bid') }
+    let(:user) { create(:user) }
+
+    context 'bad ending:' do
+      describe "when the user's balance isn't enough for the bid" do
+        let(:user) { create(:user, balance: 0) }
+        let(:amount) { 9999 }
+        let(:auction) { create(:auction) }
+
+        it "doesn't create the bid and returns an error" do
+          expect { subject.save }.not_to(change { Transaction.all.count })
+          expect(subject.errors.full_messages.first).to eq('Amount is greater than your current balance')
+        end
+      end
+
+      describe 'when the auction is already over' do
+        let(:auction) do
+          a = build(:auction, start_date: Time.zone.now - 1.hour, end_date: Time.zone.now - 1.second)
+          a.save(validate: false)
+          a
+        end
+        let(:amount) { auction.minimal_bid }
+
+        it "doesn't create the bid and returns an error" do
+          expect { subject.save }.not_to(change { Transaction.all.count })
+          expect(subject.errors.full_messages.first).to eq("Auction is already over. You can't place bids in it anymore")
+        end
+      end
+
+      describe 'when the minimal bid is not reached' do
+        let(:auction) { create(:auction) }
+        let(:amount) { auction.minimal_bid - 1 }
+
+        it "doesn't create the bid and returns an error" do
+          expect { subject.save }.not_to(change { Transaction.all.count })
+          expect(subject.errors.full_messages.first).to eq("Amount is less than the minimal bid setted for this auction: #{auction.minimal_bid}")
+        end
+      end
+
+      describe 'when the bid does not surpass the winning bid value for at least 10' do
+        let(:auction) { create(:auction) }
+        let(:user2) { create(:user) }
+        let(:amount) { 200 }
+
+        before do
+          create(:transaction, :bid, amount: auction.minimal_bid, receiver: auction, giver: user2)
+        end
+
+        it "doesn't create the bid and returns an error" do
+          expect { subject.save }.not_to(change { Transaction.all.count })
+          expect(subject.errors.full_messages.first).to eq("Amount needs to be greater than #{auction.winning_bid.amount + 10} to cover the currently winning bid")
+        end
+      end
     end
   end
 end
