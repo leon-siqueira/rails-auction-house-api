be8b250aa6f5ae1535085e91f7245a34a4d96bfa
Leon Siqueira <leon.siqueir4@gmail.com>
2024-04-19 18:56:38 -0300

    Start and end auction jobs unit tests (#3)
    * test: start and end auction jobs created
    
    * lint
    
    * fix: unupdated variable

---
+++ spec/jobs/end_auction_job_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe EndAuctionJob, type: :job do
+  include ActiveJob::TestHelper
+  subject(:end_auction) { described_class.perform_now(auction) }
+
+  describe '#perform' do
+    let(:auction) { create(:auction) }
+    let(:original_art_owner) { auction.art.owner }
+
+    context 'when the auction has no bids' do
+      it 'ends the auction with no further consequences' do
+        expect { end_auction }.to change(auction, :status).from('in_progress').to('finished')
+        expect(auction.reload.art.owner).to eq(original_art_owner)
+      end
+    end
+
+    context 'when the auction has bids' do
+      let(:loser_bid) { create(:transaction, :bid, receiver: auction, amount: 100) }
+      let(:winner_bid) { create(:transaction, :bid, receiver: auction, amount: 200) }
+
+      it 'ends the auction, repays bid value to the auction owner and transfers the art to the highest bidder' do
+        expect { end_auction }.to change(auction, :status).from('in_progress').to('finished')
+                              .and change(Transaction, :count).by(1)
+                              .and change(auction.art, :owner).from(original_art_owner).to(winner_bid.giver)
+      end
+    end
+  end
+end
