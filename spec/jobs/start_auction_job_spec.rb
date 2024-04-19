df3d4ee7f3a462a4e0fd0d0ee2f1ba3e96c23cc2
Leon Siqueira <leon.siqueir4@gmail.com>
2024-04-19 18:44:11 -0300

    test: start and end auction jobs created

---
+++ spec/jobs/start_auction_job_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe StartAuctionJob, type: :job do
+  include ActiveJob::TestHelper
+  subject(:start_auction) { described_class.perform_now(auction) }
+
+  describe '#perform' do
+    let(:auction) { create(:auction, status: :scheduled, start_date: 1.second.from_now) }
+
+    it 'starts the auction' do
+      expect { start_auction }.to change(auction, :status).from('scheduled').to('in_progress').and enqueue_job(EndAuctionJob)
+    end
+  end
+end
